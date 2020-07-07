6bb75b08eb73c64b924a4e4966c38e75a97ea792
Dipen_Maharjan <edwinmhzn@gmail.com>
2020-07-07 22:24:49 +0545

    commit

---
+++ precache-manifest.8e9299949fb242dfd2189b58ae13cfd9.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6db2d0a5bae76abf4b5dda442fc10f30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c60a09da7b38240f705",
+    "url": "/static/css/2.32693d01.chunk.css"
+  },
+  {
+    "revision": "9d0473692d46c7bff9ee",
+    "url": "/static/css/main.b413f4d4.chunk.css"
+  },
+  {
+    "revision": "5c60a09da7b38240f705",
+    "url": "/static/js/2.b9f085fd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.b9f085fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d0473692d46c7bff9ee",
+    "url": "/static/js/main.a66b1450.chunk.js"
+  },
+  {
+    "revision": "7380a0fc83a939c2c0a7",
+    "url": "/static/js/runtime-main.ffc1db4e.js"
+  },
+  {
+    "revision": "e421d8303228bd3ed716cc6018616240",
+    "url": "/static/media/arrowright.e421d830.svg"
+  },
+  {
+    "revision": "ae4d32c8af06411a087882e769aa5701",
+    "url": "/static/media/box.ae4d32c8.svg"
+  },
+  {
+    "revision": "f329df9df58cb5841215fdfe2c1c66a6",
+    "url": "/static/media/byConstruction.f329df9d.svg"
+  },
+  {
+    "revision": "393a27a33d9e3a192c973c3435b7c2d1",
+    "url": "/static/media/circle.393a27a3.svg"
+  },
+  {
+    "revision": "67269fbda1497efceed041dfdc193691",
+    "url": "/static/media/download.67269fbd.svg"
+  },
+  {
+    "revision": "02cc09fc1d0ee0c347b01336f274ab56",
+    "url": "/static/media/facebook-logo.02cc09fc.svg"
+  },
+  {
+    "revision": "9a1f8b1dcac11402f6c92f559ac274fc",
+    "url": "/static/media/greyarrow.9a1f8b1d.svg"
+  },
+  {
+    "revision": "73fe08f1f1529fbbbd5c2afa03591ec2",
+    "url": "/static/media/header.73fe08f1.png"
+  },
+  {
+    "revision": "5d49ef87c0313a38cdb47f1e2bf9562c",
+    "url": "/static/media/home.5d49ef87.svg"
+  },
+  {
+    "revision": "544e3f3958ae1db19c065fcfa1eed17d",
+    "url": "/static/media/iphone1.544e3f39.png"
+  },
+  {
+    "revision": "e5e60c3f9ad5d5c4e49d733d7154e5b9",
+    "url": "/static/media/iphone2.e5e60c3f.png"
+  },
+  {
+    "revision": "d1e7f3125b50652ff9e03ab6992bbd82",
+    "url": "/static/media/iphone3.d1e7f312.png"
+  },
+  {
+    "revision": "1c072e01d4b7eaf2dbca9543369e1160",
+    "url": "/static/media/iphonecopy2.1c072e01.png"
+  },
+  {
+    "revision": "9c2fb9e569ad2821a0a3722626036512",
+    "url": "/static/media/linkedin-logo (1).9c2fb9e5.svg"
+  },
+  {
+    "revision": "30d9dfedde48310287b662aa29bf23eb",
+    "url": "/static/media/photo.30d9dfed.png"
+  },
+  {
+    "revision": "ceece857e40c10483c89e7bdea0b14ec",
+    "url": "/static/media/pinterest.ceece857.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b6f0bc31dc4179e325f1be05b2b44b9d",
+    "url": "/static/media/star.b6f0bc31.svg"
+  },
+  {
+    "revision": "50b1dee1c079df279a2c1266fb6c0234",
+    "url": "/static/media/twitter.50b1dee1.svg"
+  },
+  {
+    "revision": "326cf52746aa121af681f188b9f1bf09",
+    "url": "/static/media/uniqueapp.326cf527.svg"
+  },
+  {
+    "revision": "bd94ecd8458258a5d5d3d5af3f919982",
+    "url": "/static/media/usercircle.bd94ecd8.svg"
+  },
+  {
+    "revision": "c90680701673fd422289a9e397f80177",
+    "url": "/static/media/whitearrow.c9068070.svg"
+  }
+]);
